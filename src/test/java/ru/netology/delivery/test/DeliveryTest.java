d1b037b85485aa8af2ef0022aff91352008b9f76
Ekaterina Kobeleva <kobelevaekaterina09@gmail.com>
2023-09-03 15:33:10 +0300

    Rename DeliveryTest

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -42,7 +42,7 @@ class DeliveryTest {
         $("[data-test-id='replan-notification'] .notification__content")
                 .shouldHave(Condition.text("У вас уже запланирована встреча на другую дату. Перепланировать?"))
                 .shouldBe(Condition.visible);
-        $("[data-test-id='replan-notification] button").click();
+        $("[data-test-id='replan-notification'] button").click();
         $("[data-test-id='success-notification'] .notification__content")
                 .shouldHave(Condition.exactText("Встреча успешно запланирована на " + secondMeetingDate))
                 .shouldBe(Condition.visible);
